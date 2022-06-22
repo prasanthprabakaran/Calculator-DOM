b7428b69cbb67d7c1b9f33986946361ca8645822
prasanthprabakaran <iprasanthpraba@gmail.com>
2022-06-22 15:22:08 +0530

    functionality

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ let temp_Result = createClass("temp-result");
 let all_Button = createClass("all_button");
 let btnC = createBtn("all-clear","C");
 let btnCE = createBtn("last-entity-clear","CE");
-let btnDot = createBtn("operation",".");
-let btnX = createBtn("operation","X");
+let btnMod = createBtn("operation","%");
+let btnX = createBtn("operation","x");
 let btn7 = createBtn("number","7");
 let btn8 = createBtn("number","8");
 let btn9 = createBtn("number","9");
@@ -26,16 +26,16 @@ let btn2 = createBtn("number","2");
 let btn3 = createBtn("number","3");
 let btnAdd = createBtn("operation","+");
 let btn0 = createBtn("number","0");
-let btn00 = createBtn("number","00");
-let btnEqual = createBtn2("operation","btnEqual","=");
+let btnDot = createBtn2("number","dot",".");
+let btnEqual = createBtn2("equal","btnEqual","=");
 
 display.append(display_1,display_2,temp_Result);
 all_Button.append(
-    btnC,btnCE,btnDot,btnX,
+    btnC,btnCE,btnMod,btnX,
     btn7,btn8,btn9,btnSlash,
     btn4,btn5,btn6,btnMinus,
     btn1,btn2,btn3,btnAdd,
-    btn0,btn00,btnEqual);
+    btn0,btnDot,btnEqual);
 
 calculator.append(display,all_Button);
 container.append(calculator);
@@ -71,3 +71,144 @@ function createBtn2(clsName2,clsName3,value){
 };
 
 
+
+const display1El = document.querySelector('.display_1');
+const display2El = document.querySelector('.display_2');
+const tempResultEl = document.querySelector('.temp-result');
+const numbersEl = document.querySelectorAll('.number');
+const operationEl = document.querySelectorAll('.operation');
+const equalEl = document.querySelector('.equal');
+const clearAllEl = document.querySelector('.all-clear');
+const clearLastEl = document.querySelector(".last-entity-clear");
+
+let dis1Num = "";
+let dis2Num = "";
+let result = null;
+let lastOperation = "";
+let haveDot = false;
+
+numbersEl.forEach((number) => {
+    number.addEventListener("click", (e) => {
+      if (e.target.innerText === "." && !haveDot) {
+        haveDot = true;
+      } else if (e.target.innerText === "." && haveDot) {
+        return;
+      }
+      dis2Num += e.target.innerText;
+      display2El.innerText = dis2Num;
+      // console.log();
+    });
+});
+
+operationEl.forEach((operation) => {
+    operation.addEventListener("click",(e) => {
+        if (!dis2Num) return;
+        haveDot = false;
+        const operationName = e.target.innerText;
+        if (dis1Num && dis2Num && lastOperation) {
+            mathOperation();
+        } else {
+            result = parseFloat(dis2Num);
+        }
+        clearVar(operationName);
+        lastOperation = operationName;
+        console.log(result);
+        
+    });
+});
+
+function clearVar (name = "") {
+    dis1Num += dis2Num + " " + name + " ";
+    display1El.innerText = dis1Num;
+    display2El.innerText = "";
+    dis2Num = "";
+    tempResultEl.innerText = result;
+};
+
+
+function mathOperation() {
+    if (lastOperation === "x") {
+        result = parseFloat(result) * parseFloat(dis2Num);
+    } else if (lastOperation === "+") {
+        result = parseFloat(result) + parseFloat(dis2Num);
+    } else if (lastOperation === "-") {
+        result = parseFloat(result) - parseFloat(dis2Num);
+    } else if (lastOperation === "/") {
+        result = parseFloat(result) / parseFloat(dis2Num);
+    } else if (lastOperation === "%") {
+        result = parseFloat(result) % parseFloat(dis2Num);
+    }
+}
+
+equalEl.addEventListener("click", ()=>{
+    if (!dis2Num || !dis1Num) return;
+    haveDot = false;
+    mathOperation();
+    clearVar();
+    display2El.innerText = result;
+    tempResultEl.innerText = "";
+    dis2Num = result;
+    dis1Num = "";
+});
+
+clearAllEl.addEventListener("click", () => {
+    dis1Num = "";
+    dis2Num = "";
+    display1El.innerText = "";
+    display2El.innerText = "";
+    result = "";
+    tempResultEl.innerText = "";
+});
+
+clearLastEl.addEventListener("click", () => {
+    display2El.innerText = "";
+    dis2Num = "";
+});
+
+window.addEventListener("keydown",(e) => {
+    if (
+        e.key === "0" ||
+        e.key === "1" ||
+        e.key === "2" ||
+        e.key === "3" ||
+        e.key === "4" ||
+        e.key === "5" ||
+        e.key === "6" ||
+        e.key === "7" ||
+        e.key === "8" ||
+        e.key === "9" ||
+        e.key === "."
+    ) {
+        clickButtonEl(e.key);
+        console.log(e.key)
+    } else if (e.key ==="+" || e.key === "-" ||e.key === "/" || e.key === "%"){
+        clickOperation(e.key);
+    } else if (e.key === "*") {
+        clickOperation("x");
+    } else if (e.key === "Enter" || e.key ==="=") {
+        clickEqual();
+    } else {
+        alert("enter only numbers");
+    }
+
+});
+
+function clickButtonEl(key) {
+    numbersEl.forEach((button) => {
+        if (button.innerText === key) {
+            button.click();
+        }
+    });
+};
+
+function clickOperation(key) {
+    operationEl.forEach((operation) => {
+        if (operation.innerText === key) {
+            operation.click();
+        }
+    });
+};
+
+function clickEqual() {
+    equalEl.click();
+};
